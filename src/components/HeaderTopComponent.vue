0ce237d5258499407ae3c26ba858287d3152f0a2
Borrellsss <edoardofilippo.borrello@gmail.com>
2022-07-15 13:39:15 +0200

    font-awesome import+header-top done

--- src/components/HeaderTopComponent.vue
+++ src/components/HeaderTopComponent.vue
@@ -1,6 +1,58 @@
 <template>
-    <div>
-        
+    <div class="header-top">
+        <div class="container">
+            <div class="flex justify-between">
+                <div class="header-top-left">
+                    <span>
+                        <span>
+                            <i class="fa-solid fa-clock"></i>
+                        </span>
+                        Open Hours: Mon - Sat - 9:00 - 18:00
+                    </span>
+                </div>
+                <div class="header-top-right">
+                    <ul class="contacts flex">
+                        <li>
+                            <a href="#">
+                                <span>
+                                    <i class="fa-solid fa-phone"></i>
+                                </span>
+                                +1 (305) 1234-5678
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span>
+                                    <i class="fa-solid fa-envelope"></i>
+                                </span>
+                                hello@example.com
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span>
+                                    <i class="fa-brands fa-facebook-f"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span>
+                                    <i class="fa-brands fa-twitter"></i>
+                                </span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#">
+                                <span>
+                                    <i class="fa-brands fa-linkedin-in"></i>
+                                </span>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -18,5 +70,18 @@ export default {
 @import "../style/variables.scss";
 
 // *STYLING
-
+.header-top {
+    padding-block: $header-top-padding;
+    font-size: $header-top-font-size;
+    color: #c0c3d0;
+    background-color: $twentieth-color;
+}
+.contacts > li {
+    margin-left: 1.5rem;
+}
+.fa-clock,
+.fa-phone,
+.fa-envelope {
+    margin-right: 0.15rem;
+}
 </style>
